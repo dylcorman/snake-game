042fe4cb856313988ad3a8b56a4bdaa38dcf969b
dylcorman <dylancorman8@gmail.com>
2023-04-05 01:47:53 -0400

    added: css styling/began implementing actual gameplay.

--- app.js
+++ app.js
@@ -1,85 +1,8 @@
-/* //Define variables
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-const box = 32;
-let snake = [];
-snake[0] = {
-    x: 9 * box,
-    y: 10 * box
-};
-
-//Create food object/define inital position
-let food = {
-    x: Math.floor(Math.random() * 17 + 1) * box,
-    y: Math.floor(Math.random() * 15 + 3) * box
-};
-
-//Define snake's inital direction
-let d; 
-document.addEventListener('keydown', direction);
-
-function direction(event) {
-    if (event.keyCode === 37 && d !== 'RIGHT') {
-        d = 'LEFT';
-    } else if (event.keyCode === 38 && d !== 'DOWN') {
-        d = 'UP';
-    } else if (event.keyCode === 39 && d !== 'LEFT') {
-        d = 'RIGHT';
-    } else if (event.keyCode === 40 && d !== 'UP') {
-        d = 'DOWN';
-    }
-}
-
-//Create collision factor 
-function collision(head, array) {
-    for (let i = 0; i < array.length; i++) {
-        if (head.x === array[i].x && head.y === array[i].y) {
-            return true;
-        }
-    }
-    return false;
-}
-
-//Function to draw the snake and food objects
-function draw() { 
-    ctx.fillStyle = 'lightblue';
-    ctx.fillRect(box, 3 * box, 17 * box, 17 * box);
-
-    for (let i = 0; i < snake.length; i++) {
-        ctx.fillStyle = (i === 0) ? 'green' : 'white';
-        ctx.fillRect(snake[i].x, snake[i].y, box, box);
-
-        ctx.strokeStyle = 'red';
-        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
-    }
-}
-
-//Update game/detect collisions
-let snakeX = snake[0].x;
-let snakeY = snake[0].y;
-
-if (d === 'LEFT') snakeX -= box;
-if (d === 'UP') snakeY -= box;
-if (d === 'RIGHT') snakeX += box;
-if (d === 'DOWN') snakeY += box;
-
-if (snakeX === food.x && snakeY === food.y) {
-    food = {
-        x: Math.floor(Math.random() * 17 + 1) * box,
-        y: Math.floor(Math.random() * 15 + 3) * box
-    };
-} else {
-    snake.pop();
-}
-
-let newHead = {
-    x: snakeX,
-    y: snakeY
-}; */
 
 const canvas = document.getElementById('gameCanvas');
-const snakeGame = new SnakeGame(canvas);
-let game = setInterval(() => snakeGame.draw(), 100);
+const scoreValue = document.getElementById('scoreValue');
+const game = new SnakeGame(canvas, scoreValue);
+
 
 
 
--- snakeGame.js
+++ snakeGame.js
@@ -1,5 +1,5 @@
 class SnakeGame {
-    constructor(canvas) {
+    constructor(canvas, scoreElement) {
         this.canvas = canvas;
         this.ctx = canvas.getContext('2d');
         this.box = 32;
@@ -16,6 +16,9 @@ class SnakeGame {
 
         this.d = null;
         document.addEventListener('keydown', (event) => this.direction(event));
+
+        this.score = 0;
+        this.scoreElement = scoreElement;
     }
 
     direction(event) {
@@ -50,6 +53,40 @@ class SnakeGame {
             this.ctx.strokeStyle = 'red';
             this.ctx.strokeRect(this.snake[i].x, this.snake[i].y, this.box, this.box);
         }
+
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(this.food.x, this.food.y, this.box, this.box);
+
+        let snakeX = this.snake[0].x;
+        let snakeY = this.snake[0].y;
+
+        if (this.d === 'LEFT') snakeX -= this.box;
+        if (this.d === 'UP') snakeY -= this.box;
+        if (this.d === 'RIGHT') snakeX += this.box;
+        if (this.d === 'DOWN') snakeY += this.box;
+
+        if (snakeX === this.food.x && snakeY === this.food.y) {
+            this.food = {
+                x: Math.floor(Math.random() * 17 + 1) * this.box,
+                y: Math.floor(Math.random() * 15 + 3) * this.box
+            };
+            this.score++;
+            this.scoreElement.textContent = this.score;
+        } else {
+            this.snake.pop();
+        }
+
+        let newHead = {
+            x: snakeX,
+            y: snakeY
+        };
+
+        if (snakeX < this.box || snakeX > 17 * this.box || snakeY < 3 * this.box || snakeY > 17 * this.box || this.collision(newHead, this.snake)) {
+            clearInterval(this.game);
+        }
+
+        this.snake.unshift(newHead);
     }
 }
+
 
