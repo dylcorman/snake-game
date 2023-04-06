98aaf9797989ce07aa18587cc6f4b64480e5075b
dylcorman <dylancorman8@gmail.com>
2023-04-06 00:46:50 -0400

    feat: added wasd key inputs and made snake movement smoother

--- snakeGame.js
+++ snakeGame.js
@@ -14,25 +14,27 @@ class SnakeGame {
         this.appleY = 5;
         this.score = 0;
         this.scoreElement = scoreElement;
-        this.speed = 7;
+        this.speed = 10;
+        this.keyDown = this.keyDown.bind(this);
         document.body.addEventListener('keydown', (event) => this.keyDown(event));
     }
 
     keyDown(event) {
-        if (event.keyCode === 38 && this.yVelocity !== 1) {
+        if ((event.keyCode === 38 || event.keyCode === 87) && this.yVelocity !== 1) { // Add "W" key (keyCode 87)
             this.yVelocity = -1;
             this.xVelocity = 0;
-        } else if (event.keyCode === 40 && this.yVelocity !== -1) {
+        } else if ((event.keyCode === 40 || event.keyCode === 83) && this.yVelocity !== -1) { // Add "S" key (keyCode 83)
             this.yVelocity = 1;
             this.xVelocity = 0;
-        } else if (event.keyCode === 37 && this.xVelocity !== 1) {
+        } else if ((event.keyCode === 37 || event.keyCode === 65) && this.xVelocity !== 1) { // Add "A" key (keyCode 65)
             this.yVelocity = 0;
             this.xVelocity = -1;
-        } else if (event.keyCode === 39 && this.xVelocity !== -1) {
+        } else if ((event.keyCode === 39 || event.keyCode === 68) && this.xVelocity !== -1) { // Add "D" key (keyCode 68)
             this.yVelocity = 0;
             this.xVelocity = 1;
         }
     }
+
 
     drawGame() {
         this.changeSnakePosition();
