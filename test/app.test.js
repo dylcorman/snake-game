9bfad4a9bc0b8d4015c8519c508a76505ca5822a
dylcorman <dylancorman8@gmail.com>
2023-04-05 22:57:03 -0400

    fixed some tests

--- test/app.test.js
+++ test/app.test.js
@@ -13,24 +13,67 @@ describe('SnakeGame', () => {
         expect(snakeGame).toBeDefined();
     });
 
-    it('should initialize the snake with one segment', () => {
-        expect(snakeGame.snake.length).toBe(1);
+    it('should initialize the snake with tailLength of 2', () => {
+        expect(snakeGame.tailLength).toBe(2);
     });
 
     it('should initialize the food object', () => {
-        expect(snakeGame.food).toBeDefined();
+        expect(snakeGame.appleX).toBeDefined();
+        expect(snakeGame.appleY).toBeDefined();
     });
 
-    it('should not detect a collision with an empty array', () => {
-        const head = { x: 1, y: 1 };
-        const array = [];
-        expect(snakeGame.collision(head, array)).toBe(false);
+    it('should not detect a game over with an empty array', () => {
+        snakeGame.headX = 1;
+        snakeGame.headY = 1;
+        snakeGame.snakeParts = [];
+        expect(snakeGame.isGameOver()).toBe(false);
     });
 
-    it('should detect a collision with a matching coordinate in the array', () => {
-        const head = { x: 1, y: 1 };
-        const array = [{ x: 1, y: 1 }];
-        expect(snakeGame.collision(head, array)).toBe(true);
+    it('should detect a game over with a matching coordinate in the array', () => {
+        snakeGame.headX = 1;
+        snakeGame.headY = 1;
+        snakeGame.snakeParts = [{ x: 1, y: 1 }];
+        expect(snakeGame.isGameOver()).toBe(true);
+    });
+
+    it('should update the direction of the snake', () => {
+        snakeGame.keyDown({ keyCode: 37 });
+        expect(snakeGame.xvelocity).toBe(-1);
+        expect(snakeGame.yvelocity).toBe(0);
+        snakeGame.keyDown({ keyCode: 38 });
+        expect(snakeGame.xvelocity).toBe(0);
+        expect(snakeGame.yvelocity).toBe(-1);
+        snakeGame.keyDown({ keyCode: 39 });
+        expect(snakeGame.xvelocity).toBe(1);
+        expect(snakeGame.yvelocity).toBe(0);
+        snakeGame.keyDown({ keyCode: 40 });
+        expect(snakeGame.xvelocity).toBe(0);
+        expect(snakeGame.yvelocity).toBe(1);
+    });
+
+    it('should not allow the snake to reverse direction', () => {
+        snakeGame.keyDown({ keyCode: 37 });
+        snakeGame.keyDown({ keyCode: 39 });
+        expect(snakeGame.xvelocity).toBe(-1);
+        expect(snakeGame.yvelocity).toBe(0);
+        snakeGame.keyDown({ keyCode: 38 });
+        snakeGame.keyDown({ keyCode: 40 });
+        expect(snakeGame.xvelocity).toBe(0);
+        expect(snakeGame.yvelocity).toBe(-1);
+    });
+
+    it('should update the food position when the snake eats it', () => {
+        const initialFoodX = snakeGame.appleX;
+        const initialFoodY = snakeGame.appleY;
+        snakeGame.headX = initialFoodX;
+        snakeGame.headY = initialFoodY;
+        snakeGame.checkCollision();
+
+        expect(snakeGame.appleX).not.toBe(initialFoodX);
+        expect(snakeGame.appleY).not.toBe(initialFoodY);
     });
 });
+
+
+
 
